687b0af5a0303db80fd228df14d7e7e68fadd6ed
ridhomyers <ridhomrr0@gmail.com>
2024-06-18 04:37:32 +0700

    fix client render in home

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 import prisma from "@/libs/prismadb";
 
 export async function getSession() {
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Container from "@/components/Container";
-import ListingCard from "@/components/listings/ListingCard";
 import EmptyState from "@/components/EmptyState";
 
 import getListings, { IListingsParams } from "@/actions/getListings";
-import getCurrentUser from "@/actions/getCurrentUser";
 import ClientOnly from "@/components/ClientOnly";
+import ClientOnlyHome from "@/components/ClientOnlyHome";
 
 interface HomeProps {
   searchParams: IListingsParams;
@@ -12,7 +10,6 @@ interface HomeProps {
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -22,33 +19,7 @@ const Home = async ({ searchParams }: HomeProps) => {
     );
   }
 
-  return (
-    <ClientOnly>
-      <Container>
-        <div
-          className="
-            pt-24
-            grid 
-            grid-cols-1 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-            gap-8
-          "
-        >
-          {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
-      </Container>
-    </ClientOnly>
-  );
+  return <ClientOnlyHome listings={searchParams} />;
 };
 
 export default Home;
