0b2d7cab9b4f67e3752640d95c70b0e2e289071a
ridhomyers <ridhomrr0@gmail.com>
2024-06-18 05:34:10 +0700

    fix logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -7,15 +7,21 @@ const Logo = () => {
   const router = useRouter();
 
   return (
-    <Image
-      priority={true}
+    <div
       onClick={() => router.push("/")}
-      className="hidden md:block cursor-pointer"
-      src="/images/logo.png"
-      height="100"
-      width="100"
-      alt="Logo"
-    />
+      className="flex gap-x-2 justify-center items-center cursor-pointer"
+    >
+      <Image
+        priority={true}
+        src="/images/logo.png"
+        height="40"
+        width="40"
+        alt="Logo"
+      />
+      <div className="text-[#FF5B58] hidden md:block font-extrabold text-xl mt-1">
+        BNB Hotel
+      </div>
+    </div>
   );
 };
 
